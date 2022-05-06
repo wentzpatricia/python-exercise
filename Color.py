62e55ab3df852a7ecd3a5f48ddea61889755616a
Patricia Wentz <patriciawentzm@gmail.com>
2022-05-05 21:17:38 -0300

    :sparkles: aplicating color class in service menu

--- Color.py
+++ Color.py
@@ -1,6 +1,5 @@
 class Color: 
     cyan = "\033[1;36m"
-    green = "\033[1;32m"
     red = "\033[1;31m"
     magenta = "\033[1;35m"
     endColor = "\033[0;0m"
--- Menu.py
+++ Menu.py
@@ -1,15 +1,18 @@
+from Color import Color
+color = Color()
+
 def menu_principal():
-    return input(("\n"*4)+"""\033[1;34m
+    return input(("\n"*4)+f"""{color.magenta}
  ====================================
- ------------------------------------\033[0;0m 
-                MENU\033[1;34m
- ------------------------------------\033[0;0m
+ ------------------------------------{color.endColor} 
+                MENU{color.magenta}
+ ------------------------------------{color.endColor}
     0- Finalizar o Programa
     1- Cadastrar Estados
     2- Cadastrar Cidades
     3- Relatório de Estados
     4- Relatório de Cidades
-    5- Atualizar números de casos \033[1;34m
+    5- Atualizar números de casos {color.magenta}
  ------------------------------------
- ====================================\033[0;0m
+ ===================================={color.endColor}
     Escolha: """)
